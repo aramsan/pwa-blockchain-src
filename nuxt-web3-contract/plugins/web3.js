503f79ca2ca3ad822552dd6e769fe4b37d5d8421
Aram Mine <aram.mine@gaiax.com>
2020-12-03 01:55:48 +0900

    [nuxt-web3-contract]web3の接続方法の変更

--- nuxt-web3-contract/plugins/web3.js
+++ nuxt-web3-contract/plugins/web3.js
@@ -1,8 +1,26 @@
-import Web3 from "web3"
+import Web3 from 'web3'
 
-export default async function(context, inject) {
-  const httpEndpoint = "http://127.0.0.1:7545" //ここを変えればいろんなチェーンに繋げられます
-  const web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+export default async function (context, inject) {
+    let web3
 
-    inject('web3',web3)
+    if (
+        typeof window !== 'undefined' &&
+        typeof window.ethereum !== 'undefined'
+    ) {
+        web3 = new Web3(window.ethereum)
+        window.ethereum.enable().catch((error) => {
+            // User denied account access
+            console.log(error)
+        })
+    } else if (
+        typeof window !== 'undefined' &&
+        typeof window.web3 !== 'undefined'
+    ) {
+        web3 = new Web3(window.web3.currentProvider)
+    } else {
+        const httpEndpoint = 'http://127.0.0.1:7545'
+        web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+    }
+
+    inject('web3', web3)
 }
