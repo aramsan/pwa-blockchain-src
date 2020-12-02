fc37533e51b1a5e4e80a73e4404128c00c030428
Aram Mine <aram.mine@gaiax.com>
2020-12-03 01:58:49 +0900

    [nuxt-web3-contract]コントラクトとの接続

--- nuxt-web3-contract/plugins/web3.js
+++ nuxt-web3-contract/plugins/web3.js
@@ -1,4 +1,5 @@
 import Web3 from 'web3'
+import artifacts from "~~/build/contracts/SingleNumRegister.json"
 
 export default async function (context, inject) {
     let web3
@@ -22,5 +23,13 @@ export default async function (context, inject) {
         web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
     }
 
+    let networkId = await web3.eth.net.getId()// チェーンのネットワークIDを取得
+    let contract = new web3.eth.Contract( //コントラクトのインスタンスの初期化。設定ファイルと、アドレスが必要
+        artifacts.abi, // コントラクトのコンパイル後の設定ファイル
+        artifacts.networks[networkId].address // ネットワークIDごとに保存されているコントラクトのアドレスを読み込む
+    )
+
     inject('web3', web3)
+    inject('contract',contract) // インスタンスを生やす
+
 }
