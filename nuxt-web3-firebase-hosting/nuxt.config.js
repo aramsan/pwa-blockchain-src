66b304f259822125ca4a388fe57f2507a208de4d
Aram Mine <aram.mine@gaiax.com>
2020-12-05 22:59:58 +0900

    [nuxt-web3-firebase-hosting]web3 pluginをClient modeに

--- nuxt-web3-firebase-hosting/nuxt.config.js
+++ nuxt-web3-firebase-hosting/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~/plugins/web3.js' }
+    { src: '~/plugins/web3.js', mode: 'client' }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
